1a5f6615a350ce53dccf8c49c5cb76e9a11c4b2e
real2two <26689496+real2two@users.noreply.github.com>
2023-12-22 22:17:21 -0500

    Updated Bridge

--- apps/bridge/src/index.ts
+++ apps/bridge/src/index.ts
@@ -1,29 +1,27 @@
 import env from "@/env";
 import DiscordCrossHosting from "discord-cross-hosting";
 
-(async () => {
-  const server = new DiscordCrossHosting.Bridge({
-    port: env.BridgePort,
-    authToken: env.BridgeAuthToken,
-    totalShards: env.BridgeTotalShards,
-    totalMachines: env.BridgeTotalMachines,
-    shardsPerCluster: env.BridgeShardsPerCluster,
-    token: env.DiscordToken as string,
-  });
+const server = new DiscordCrossHosting.Bridge({
+  port: env.BridgePort,
+  authToken: env.BridgeAuthToken,
+  totalShards: env.BridgeTotalShards,
+  totalMachines: env.BridgeTotalMachines,
+  shardsPerCluster: env.BridgeShardsPerCluster,
+  token: env.DiscordToken as string,
+});
 
-  server.on("debug", console.log);
+server.on("debug", console.log);
 
-  server.on("ready", (url) => {
-    console.log(`Server is ready ${url}`);
-  });
+server.on("ready", (url) => {
+  console.log(`Server is ready ${url}`);
+});
 
-  process.on("uncaughtException", (err) => {
-    console.error(err);
-  });
+process.on("uncaughtException", (err) => {
+  console.error(err);
+});
 
-  process.on("unhandledRejection", (err) => {
-    console.error(err);
-  });
+process.on("unhandledRejection", (err) => {
+  console.error(err);
+});
 
-  await server.start();
-})();
+await server.start();

4220b84a0204106015a9b91492a3b92a08db4392
real2two <26689496+real2two@users.noreply.github.com>
2024-01-07 17:10:06 -0500

    Update guilds.ts

--- packages/utils/src/db/guilds.ts
+++ packages/utils/src/db/guilds.ts
@@ -3,27 +3,34 @@ import { db, Tables } from "@/db";
 
 export async function getGuild(guildId: bigint) {
   // Attempts to fetch guild
-  const guild = (
+  const guild = await selectGuild(guildId);
+
+  // If guild doesn't exist, create it and attempt to fetch it again
+  if (!guild) {
+    await createGuild(guildId);
+    return selectGuild(guildId);
+  };
+
+  // Return the guild
+  return guild;
+}
+
+export async function selectGuild(guildId: bigint) {
+  // Attempts to fetch guild
+  return (
     await db
       .select()
       .from(Tables.guilds)
       .where(eq(Tables.guilds.guildId, guildId))
       .limit(1)
   )[0];
-
-  // If guild doesn't exist, create it and attempt to fetch it again
-  if (!guild) {
-    await createGuild(guildId);
-    return getGuild(guildId);
-  }
-
-  // Return the guild
-  return guild;
 }
 
 export async function createGuild(guildId: bigint) {
   // Inserts the guild into the database
-  return await db.insert(Tables.guilds).values({
-    guildId,
-  });
+  return await db
+    .insert(Tables.guilds)
+    .values({
+      guildId,
+    });
 }

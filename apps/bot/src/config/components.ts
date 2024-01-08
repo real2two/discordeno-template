71bf46ef2dc8a1de5d445b0e5e418d4378b78038
Two <26689496+real2two@users.noreply.github.com>
2024-01-08 10:18:37 -0500

    Formatted code

--- apps/bot/src/config/components.ts
+++ apps/bot/src/config/components.ts
@@ -1,12 +1,15 @@
 import { globSync } from "glob";
 import { Component } from "@/discordeno-helpers";
 
-const files = globSync('./src/components/**/*.ts', { matchBase: true, nodir: true }).map(f => `../${f.slice("src/".length)}`);
+const files = globSync("./src/components/**/*.ts", {
+  matchBase: true,
+  nodir: true,
+}).map((f) => `../${f.slice("src/".length)}`);
 export const components: Component[] = [];
 
 for (const file of files) {
-    const component = (await import(file)).default;
-    if (component instanceof Component) {
-        components.push(component);
-    }
+  const component = (await import(file)).default;
+  if (component instanceof Component) {
+    components.push(component);
+  }
 }

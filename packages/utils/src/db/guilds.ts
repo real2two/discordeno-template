6322fe226eb0607ad9fec678668a8fc5af48a971
real2two <26689496+real2two@users.noreply.github.com>
2024-01-11 17:08:37 -0500

    Added comments to the guild database functions

--- packages/utils/src/db/guilds.ts
+++ packages/utils/src/db/guilds.ts
@@ -1,6 +1,11 @@
 import { eq } from "drizzle-orm";
 import { db, Tables } from "@/db";
 
+/**
+ * Get a guild or create one
+ * @param guildId The guild ID
+ * @returns The guild's information in the database
+ */
 export async function getGuild(guildId: bigint) {
   // Attempts to fetch guild
   const guild = await selectGuild(guildId);
@@ -15,6 +20,11 @@ export async function getGuild(guildId: bigint) {
   return guild;
 }
 
+/**
+ * Select a guild
+ * @param guildId The guild ID
+ * @returns The guild's information if it exists
+ */
 export async function selectGuild(guildId: bigint) {
   // Attempts to fetch guild
   return (
@@ -26,9 +36,29 @@ export async function selectGuild(guildId: bigint) {
   )[0];
 }
 
+/**
+ * Create a guild on the database
+ * @param guildId The guild ID
+ */
 export async function createGuild(guildId: bigint) {
   // Inserts the guild into the database
   return await db.insert(Tables.guilds).values({
     guildId,
   });
+}
+
+/**
+ * Updates a guild's information
+ * @param guildId The guild ID
+ * @param data The updated guild's information
+ */
+export async function updateGuild(
+  guildId: bigint,
+  data: Partial<typeof Tables.guilds.$inferSelect>,
+) {
+  // Update a guild table
+  return await db
+    .update(Tables.guilds)
+    .set(data)
+    .where(eq(Tables.guilds.guildId, guildId));
 }

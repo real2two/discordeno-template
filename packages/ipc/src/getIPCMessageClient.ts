81505196f068dbbf2c5dabc2e4896e04b20ff84c
real2two <26689496+real2two@users.noreply.github.com>
2024-01-11 17:04:34 -0500

    IPC comments

--- packages/ipc/src/getIPCMessageClient.ts
+++ packages/ipc/src/getIPCMessageClient.ts
@@ -5,6 +5,11 @@ import type { Client } from "discord-cross-hosting";
 import type { IPCMessageRequests } from "./types/IPCMessageRequests";
 import type { IPCMessageResponses } from "./types/IPCMessageResponses";
 
+/**
+ * Get the IPC message client
+ * @param client The client
+ * @returns The IPC message client
+ */
 export function getIPCMessageClient(client: Client) {
   return new IPCMessageClient<IPCMessageRequests, IPCMessageResponses>(client);
 }
--- packages/ipc/src/getIPCMessageEvent.ts
+++ packages/ipc/src/getIPCMessageEvent.ts
@@ -4,6 +4,10 @@ import type { Bot } from "@discordeno/bot";
 import type { IPCMessageRequests } from "./types/IPCMessageRequests";
 import type { IPCMessageResponses } from "./types/IPCMessageResponses";
 
+/**
+ * Get the IPC message event
+ * @returns The IPC message event
+ */
 export function getIPCMessageEvent<B extends Bot>() {
   return createIPCMessageEvent<B, IPCMessageRequests, IPCMessageResponses>();
 }

f4484329cc4e39abd2e381778d9e7194c91a8af3
real2two <26689496+real2two@users.noreply.github.com>
2024-01-14 15:15:36 -0500

    Update index.ts

--- packages/utils/src/db/index.ts
+++ packages/utils/src/db/index.ts
@@ -1,6 +1,9 @@
-// fetch = SELECT if exists, else INSERT and SELECT
-// get = SELECT
-// create = INSERT
-// update = UPDATE
+/**
+ * fetch = SELECT if exists, else INSERT and SELECT
+ * get = SELECT
+ * create = INSERT
+ * update = UPDATE
+ * delete = DELETE
+ */
 
 export * from "./guilds";
